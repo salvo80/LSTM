760fcaa0a1e12fb051d81b697954ae35ba8aa8ac
salvo <salvolupo.g@gmail.com>
2017-11-16 01:05:01 +0100

    forecast with one week data but only t-1

--- test_kendra.py
+++ test_kendra.py
@@ -1,25 +1,58 @@
 import io
-from pandas import read_json, read_csv
+
+from math import sqrt
+from numpy import concatenate
+from sklearn.preprocessing import MinMaxScaler
+from pandas import read_json, read_csv, DataFrame, concat
 from matplotlib import pyplot
+
+from keras.models import Model, Sequential
+from keras.layers import LSTM, Dense
+
+from sklearn.metrics import mean_squared_error
+
+# convert series to supervised learning
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
+	
 # load dataset
-dataset = read_json('candles_from_1510095600000_to_1510759800000',convert_dates=[0])
+dataset = read_json('candles.json',convert_dates=[0])
 
 # get only 5 rows
 #dataset = dataset.head(5)
 
+out = io.BytesIO()
+dataset.to_csv(out, encoding='utf-8')#'test.csv'
+out.seek(0)
 
-
-dataset.to_csv('test.csv')
-
-dataset = read_csv('test.csv', index_col=1)
+dataset = read_csv(out, index_col=1)
 dataset.columns = ['no','open','close','high','low','volume']
 dataset.index.name = 'date'
 dataset.drop('no', axis=1, inplace=True)
-
-print(dataset)
+dataset.sort_index(inplace=True)
 
 values = dataset.values
-
+'''#plot all#
 groups = [0, 1, 2, 3, 4]
 i = 1
 # plot each column
@@ -30,3 +63,60 @@ for group in groups:
 	pyplot.title(dataset.columns[group], y=0.5, loc='right')
 	i += 1
 pyplot.show()
+'''
+print ('size=',dataset.count())
+# ensure all data is float
+values = values.astype('float32')
+# normalize features
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+# frame as supervised learning
+reframed = series_to_supervised(scaled, 1, 1)
+
+# drop columns we don't want to predict
+reframed.drop(reframed.columns[[6,7,8,9]], axis=1, inplace=True)
+print(reframed.head())
+
+#300 train - 85 test
+# split into train and test sets
+values = reframed.values
+n_train_hours = 300
+train = values[:n_train_hours, :]
+test = values[n_train_hours:, :]
+# split into input and outputs
+train_X, train_y = train[:, :-1], train[:, -1]
+test_X, test_y = test[:, :-1], test[:, -1]
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+
+# design network
+model = Sequential()
+model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1))
+model.compile(loss='mae', optimizer='adam')
+# fit network
+history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+# plot history
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
+
+
+# make a prediction
+yhat = model.predict(test_X)
+test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+# invert scaling for forecast
+inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
+# invert scaling for actual
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+# calculate RMSE
+rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+print('Test RMSE: %.3f' % rmse)
